2dc34aaf361db39e3d522bde4c98372cc6b83e9c
Daniel Esteban <conejo@conejo.me>
2017-06-29 22:55:00 +0200

    mostly working

--- cmd/static/js/scripts.js
+++ cmd/static/js/scripts.js
@@ -28,127 +28,100 @@ var color = Chart.helpers.color;
     }
 
 
-    $.getJSON( API_PROTO + "://" + API_URI + ":" + API_PORT +"/sensor/food01-t1")
+    apiRequest("/sensor/food01-t1;food01-h1")
         .success(function( data ) {
 
+            for(var sensor in data) {
+                var current_data = [];
+                var past_data = [];
+                var l = data[sensor]["current"].length;
+                var i = 0;
+                for(i=0;i<l;i++) {
+                    var dtime = new Date(Date.parse(data[sensor]["current"][i].time));
+                    current_data.push({
+                        x: dtime,
+                        y: data[sensor]["current"][i].value
+                    });
+                }
+                var l = data[sensor]["past"].length;
+                var i = 0;
+                for(i=0;i<l;i++) {
+                    var dtime = new Date(Date.parse(data[sensor]["past"][i].time) + 24 * 3600000);
+                    past_data.push({
+                        x: dtime,
+                        y: data[sensor]["past"][i].value
+                    });
+                }
 
-            var gdata = [];
-            var gdata2 = [];
-            var l = data.length;
-            var i = 0;
-            for(i=0;i<l;i++) {
-                var dtime = Date.parse(data[i].time);
-                gdata.push({
-                    x: dtime,
-                    y: data[i].value
-                });
-                gdata2.push({
-                    x: dtime + 400,
-                    y: data[l - i - 1].value
+                var scatterChartData = {
+                    datasets: [{
+                        label: "Current",
+                        borderColor: window.chartColors.red,
+                        backgroundColor: color(window.chartColors.red).alpha(0.2).rgbString(),
+                        data: current_data
+                    }, {
+                        label: "Past",
+                        borderColor: window.chartColors.blue,
+                        backgroundColor: color(window.chartColors.blue).alpha(0.2).rgbString(),
+                        data: past_data
+                    }]
+                };
+
+                var ctx = document.getElementById(sensor).getContext("2d");
+                window.myScatter = Chart.Scatter(ctx, {
+                    data: scatterChartData,
+                    options: {
+                        scales: {
+                            xAxes: [{
+                                type: 'time',
+                            }]
+                        },
+                        tooltips: {
+                            callbacks: {
+                                label: function(tooltipItem, data) {
+                                    var t = Date.parse(data["datasets"][tooltipItem.datasetIndex]["data"][tooltipItem.index].x);
+                                    if(tooltipItem.datasetIndex==1) {
+                                        t = t - 24 * 3600000;
+                                    }
+                                    return "(" +
+                                        new Date(t).toString().substr(4,17) +
+                                        ", " +
+                                        data["datasets"][tooltipItem.datasetIndex]["data"][tooltipItem.index].y +
+                                        ")";
+                                }
+                            },
+                        },
+                    }
                 });
             }
-
-
-            var scatterChartData = {
-                datasets: [{
-                    label: "Current",
-                    borderColor: window.chartColors.red,
-                    backgroundColor: color(window.chartColors.red).alpha(0.2).rgbString(),
-                    data: gdata
-                }, {
-                    label: "Past",
-                    borderColor: window.chartColors.blue,
-                    backgroundColor: color(window.chartColors.blue).alpha(0.2).rgbString(),
-                    data: gdata2
-                }]
-            };
-
-            var ctx = document.getElementById("line").getContext("2d");
-            window.myScatter = Chart.Scatter(ctx, {
-                data: scatterChartData,
-                options: {
-                    scales: {
-                        xAxes: [{
-                            type: 'time',
-                        }]
-                    },
-                }
-            });
-
-
-        })
-        .fail(function( data ) {
-            console.log( "FAILE: " , data );
         });
 
-    $.getJSON( API_PROTO + "://" + API_URI + ":" + API_PORT +"/sensor/food01-h1")
+    apiRequest("/meta/food01-t1;food01-h1")
         .success(function( data ) {
-
-
-            var gdata = [];
-            var gdata2 = [];
-            var l = data.length;
-            var i = 0;
-            for(i=0;i<l;i++) {
-                var dtime = Date.parse(data[i].time);
-                gdata.push({
-                    x: dtime,
-                    y: data[i].value
-                });
-                gdata2.push({
-                    x: dtime + 400,
-                    y: data[l - i - 1].value
-                });
+            if(data["food01-t1"]!=undefined) {
+                if(data["food01-t1"]["max"]!=undefined) {
+                    document.getElementById("food01-t1-meta-max").innerHTML = data["food01-t1"]["max"].toFixed(1);
+                }
+                if(data["food01-t1"]["min"]!=undefined) {
+                    document.getElementById("food01-t1-meta-min").innerHTML = data["food01-t1"]["min"].toFixed(1);
+                }
             }
-
-
-            var scatterChartData = {
-                datasets: [{
-                    label: "Current",
-                    borderColor: window.chartColors.red,
-                    backgroundColor: color(window.chartColors.red).alpha(0.2).rgbString(),
-                    data: gdata
-                }, {
-                    label: "Past",
-                    borderColor: window.chartColors.blue,
-                    backgroundColor: color(window.chartColors.blue).alpha(0.2).rgbString(),
-                    data: gdata2
-                }]
-            };
-
-            var ctx2 = document.getElementById("line2").getContext("2d");
-            window.myScatter = Chart.Scatter(ctx2, {
-                data: scatterChartData,
-                options: {
-                    scales: {
-                        xAxes: [{
-                            type: 'time',
-                        }]
-                    },
+            if(data["food01-h1"]!=undefined) {
+                if(data["food01-h1"]["avg"]!=undefined) {
+                    document.getElementById("food01-h1-meta-avg").innerHTML = data["food01-h1"]["avg"].toFixed(1);
                 }
-            });
-
-
-        })
-        .fail(function( data ) {
-            console.log( "FAILE: " , data );
+            }
+          console.log("META", data);
         });
-
-
-    $.getJSON( API_PROTO + "://" + API_URI + ":" + API_PORT +"/meta/food01-t1")
-        .success(function( data ) {
-
-            console.log(data);
-
-
-        })
-        .fail(function( data ) {
-            console.log( "FAILE: " , data );
-        });
-
 
 })(jQuery);
 
-                     
+
+function apiRequest(req) {
+    return $.getJSON( API_PROTO + "://" + API_URI + ":" + API_PORT + req)
+        .fail(function( data ) {
+        console.log( "[API REQUEST FAILED]" , req, data );
+    });
+}
      
   
